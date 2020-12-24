2dae4596092b2f4d6948c8724f4805373492889b
mobmob912 <67948556+mobmob912@users.noreply.github.com>
2020-12-24 00:14:42 -0800

    Update context.go

--- takuhai-sdk-go/context.go
+++ takuhai-sdk-go/context.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 	"log"
 	"net/http"
 	"net/url"
@@ -19,6 +20,18 @@ type Context struct {
 	stepID         string
 	jobID          string
 }
+type JobInfo struct {
+	Time time.Duration
+	RAM float64
+	CPU float64
+	
+}//	 	ジョブ側からノード側に渡す実行時間などの情報
+type Content struct {
+	Body           []byte        
+	Runtime        time.Duration
+	RAM         float64
+	CPU         float64
+}
 
 func (c *Context) Bind(v interface{}) error {
 	if err := json.NewDecoder(c.Request.Body).Decode(&v); err != nil {
@@ -27,11 +40,29 @@ func (c *Context) Bind(v interface{}) error {
 	return nil
 }
 
-func (c *Context) Next(body []byte) {
+func (c *Context) Next(body []byte, m JobInfo) {
 	cli := http.DefaultClient
 	u := *c.managerAddr
 	u.Path = fmt.Sprintf("/workflows/%s/steps/%s/next", c.workflowID, c.stepID)
-	req, err := http.NewRequest(http.MethodPost, u.String(), bytes.NewReader(body))
+	m.RAM = m.RAM
+	log.Printf("context runtime") 
+	log.Println(m.Time)
+	log.Printf("context ram")
+	log.Println(m.RAM)
+	log.Printf("context cpu")
+	log.Println(m.CPU)
+	info := &Content {
+	Body: body,
+	Runtime: m.Time,
+	RAM: m.RAM,
+	CPU: m.CPU,
+	}
+		reqBody, err := json.Marshal(&info)
+		
+		if err != nil {
+		log.Println(err)
+	}
+	req, err := http.NewRequest(http.MethodPost, u.String(), bytes.NewReader(reqBody))
 	if err != nil {
 		log.Println(err)
 		return
